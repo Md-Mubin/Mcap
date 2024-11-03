6e5d10e653f0e42ddbf2383f75f8a0b5361c4031
Md Mubin <fontdevmubin@gmail.com>
2024-11-03 19:36:42 +0600

    Changes to be committed: 	new file:   src/Component/Company_Logos/Company_logo.css 	modified:   src/Component/Company_Logos/Company_logo.jsx 	new file:   src/Component/Footer/Footer.css 	modified:   src/Component/Footer/Footer.jsx 	new file:   src/Component/Works/Works.css 	modified:   src/Component/Works/Works.jsx

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Footer.css'
 
 const Footer = () => {
   return (
     <>
-      <footer className='w-full pt-[116px] py-[120px] bg-raja'>
+      <footer>
         <div className="container">
           <img src="images/Logo.png" alt="logo_image" className='m-auto' />
 
-          <p className='font-inter font-normal text-[16px] text-[#fff] leading-[26px] mt-4 text-center'>© 2022 Elliana Mary  All Rights Reserved</p>
+          <p className='licence'>© 2022 Elliana Mary All Rights Reserved</p>
 
           <div className="border w-1/2 m-auto border-[#818181] opacity-30 mt-12"></div>
 
-          <ul className='py-9 font-inter font-normal text-[16px] text-[#fff] leading-[26px] flex justify-center gap-[45px]'>
-            <Link to={"#"} className='hover:text-shenapoti duration-300'>About</Link>
-            <Link to={"#"} className='hover:text-shenapoti duration-300'>Project</Link>
-            <Link to={"#"} className='hover:text-shenapoti duration-300'>Blog</Link>
-            <Link to={"#"} className='hover:text-shenapoti duration-300'>Pricing</Link>
-            <Link to={"#"} className='hover:text-shenapoti duration-300'>Studio</Link>
-            <Link to={"#"} className='hover:text-shenapoti duration-300'>Contact</Link>
+          <ul className='footerLinks'>
+            <Link to={"#"} >About</Link>
+            <Link to={"#"} >Project</Link>
+            <Link to={"#"} >Blog</Link>
+            <Link to={"#"} >Pricing</Link>
+            <Link to={"#"} >Studio</Link>
+            <Link to={"#"} >Contact</Link>
           </ul>
 
           <div className="border w-1/2 m-auto border-[#818181] opacity-30"></div>
 
-          <ul className='mt-9 flex gap-20 justify-center font-cormorant font-medium text-lg text-[#fff] leading-[26px]'>
-            <a href='#' className='hover:text-shenapoti duration-300'>Facebook</a>
-            <a href='#' className='hover:text-shenapoti duration-300'>Twitter</a>
-            <a href='#' className='hover:text-shenapoti duration-300'>Instagram</a>
-            <a href='#' className='hover:text-shenapoti duration-300'>Linkedin</a>
+          <ul className='footerSocials'>
+            <a href='#' >Facebook</a>
+            <a href='#' >Twitter</a>
+            <a href='#' >Instagram</a>
+            <a href='#' >Linkedin</a>
           </ul>
         </div>
       </footer>
--- src/Component/Works/Works.jsx
+++ src/Component/Works/Works.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import CommonLinks from '../../Commons/CommonLinks'
 import { Link } from 'react-router-dom'
+import './Works.css'
 
 const Works = () => {
 
     return (
         <>
-            <section className='py-[120px]'>
+            <section className='worksSection'>
                 <div className="container">
-                    <h1 className='w-[450px] text-center m-auto font-cormorant font-medium text-raja text-[80px] leading-[70px]'>Discover my featured <span className='italic underline underline-offset-8 decoration-[5px]'>works</span></h1>
-                    <ul className='w-[600px] m-auto mt-12 mb-16 flex justify-center gap-12 font-inter font-normal text-raja text-lg leading-[36px]'>
-                        <li className=' cursor-pointer line-through decoration-2 decoration-shenapoti font-bold duration-300'>All</li>
-                        <li className=' cursor-pointer hover:font-bold duration-300'>Maternity</li>
-                        <li className=' cursor-pointer hover:font-bold duration-300'>Portrait</li>
-                        <li className=' cursor-pointer hover:font-bold duration-300'>Travel</li>
-                        <li className=' cursor-pointer hover:font-bold duration-300'>Weeding</li>
-                        <li className=' cursor-pointer hover:font-bold duration-300'>Others</li>
+                    <h1>Discover my featured <span>works</span></h1>
+                    <ul className='worksRow'>
+                        <li>All</li>
+                        <li>Maternity</li>
+                        <li>Portrait</li>
+                        <li>Travel</li>
+                        <li>Weeding</li>
+                        <li>Others</li>
                     </ul>
                     <ul className='mt-[64px] font-oswald font-medium text-xl text-raja leading-[30px] flex gap-[53px]'>
                         <li className='w-[300px] h-[350px] bg-[#C4C4C4] relative mt-[112px]'><span className='absolute bottom-5 right-[-16px]'>Headshot</span></li>
